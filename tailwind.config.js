f25aca621b7037891b58e8e7adbc69d59269b6a5
Nahuel <nahuelcarlos150@gmail.com>
2024-08-27 11:27:17 -0300

    pantalla-carga

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
   content: ["./index.html", "./src/**/*.js"],
   theme: {
   extend: {
-    fontFamily: {
-      sans: ['Montserrat', 'sans-serif'],
-    },
-    backgroundImage: {
-      'custom-svg': "url('src/fondoDeLogin.svg')",
-    }
   },
   },
   plugins: [],
