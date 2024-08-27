fa7bee124ee46613398e1fc339799789098eee53
Nahuel <nahuelcarlos150@gmail.com>
2024-08-27 10:09:38 -0300

    Se hizo maqueta login

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.js"],
   theme: {
-  extend: {},
+  extend: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    backgroundImage: {
+      'custom-svg': "url('src/fondoDeLogin.svg')",
+    }
+  },
   },
   plugins: [],
   };
